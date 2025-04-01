443d0b4f12a0542a9d229acb1480a954af99fd75
Pauwo <rhernandez35@my.bcit.ca>
2025-03-31 18:29:01 -0700

    edit

--- storage/app.py
+++ storage/app.py
@@ -210,7 +210,7 @@ def get_passenger_checkins(start_timestamp, end_timestamp):
 
 # Connexion app setup
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml", base_path="/receiver", strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", base_path="/storage", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     setup_kafka_thread()
